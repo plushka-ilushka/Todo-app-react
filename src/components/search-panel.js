ad18904ecb9bd5a1d60cb3a113de41800750fe41
Makarevich Ilya <55577801+plushka-ilushka@users.noreply.github.com>
2019-12-09 22:56:00 +0300

    todo

---
+++ src/components/search-panel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+    <input type = "text" 
+        placeholder = "Type to search"
+        className = "form-control search-input"/>
+    );
+};
+
+export default SearchPanel;
